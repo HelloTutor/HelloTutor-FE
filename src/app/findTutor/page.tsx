daa1af24c8c34b98d5a839c032fb10e6e43b8bf6
suhyunH <87814333+suhyunH@users.noreply.github.com>
2023-12-25 19:21:46 +0900

    feat: nav menu 폴더 생성

---
+++ src/app/findTutor/page.tsx
@@ -0,0 +1,3 @@
+export default function FindTutor() {
+    return <></>;
+}
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,15 +1,17 @@
+import Link from 'next/link';
+
 function NavBar(){
     return(
-        <div className='h-[96px] flex justify-between items-center text-xl font-medium'>
+        <nav className='h-[96px] flex justify-between items-center text-xl font-medium'>
             {/* menu left */}
         <div className='flex'>
             {/* home icon */}
             <p className='mr-[32px]'>HelloTutor</p>
             {/* menu list */}
             <ul className='flex gap-x-6 min-w-[290px]'>
-                <li>질문하기</li>
-                <li>tutor 찾기</li>
-                <li>자유게시판</li>
+                <li><Link href="/question">질문하기</Link></li>
+                <li><Link href="/findTutor">tutor 찾기</Link></li>
+                <li><Link href="/dashboard">자유게시판</Link></li>
             </ul>
         </div>
         {/* menu right - sign in/up button */}
@@ -17,7 +19,7 @@ function NavBar(){
             <button>로그인</button>
             <button>회원가입</button>
         </div>
-        </div>
+        </nav>
     )
 }
 
