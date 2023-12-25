16700c0ed284806e93b7d2b9e0019ffcbeb93c76
suhyunH <87814333+suhyunH@users.noreply.github.com>
2023-12-25 19:13:00 +0900

    refactor: navbar layout 파일에 병합

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,4 @@
-import NavBar from '@/components/NavBar';
-import Image from 'next/image';
 
 export default function Home() {
-  return <div className='max-w-screen-lg mx-auto'>
-  <NavBar />
-  
-  </div>;
+  return <></>;
 }
