749970c098d71a657dc7697353223612babc8e06
suhyunH <87814333+suhyunH@users.noreply.github.com>
2023-12-29 23:06:32 +0900

    refactor: label과 input 분리

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,3 +1,4 @@
+import SubmitButton from '@/components/SubmitButton';
 import Link from 'next/link';
 
 export default function Signin() {
@@ -6,15 +7,15 @@ export default function Signin() {
     <div className='max-w-[970px] mx-auto mt-[160px]'>
     {/* 로그인 form */}
         <form className='flex flex-col gap-y-6 max-w-[400px] mx-auto'>
-            <label>
-                <p className='text-lg mb-4'>이메일</p>
-                <input className='border-2 border-[#d9d9d9] rounded-lg h-[64px] px-4 w-[400px]' type="email" name="email" placeholder='이메일을 입력하세요.'/>
-            </label>
-            <label>
-                <p className='text-lg mb-4'>비밀번호</p>
-                <input className='border-2 border-[#d9d9d9] rounded-lg h-[64px] px-4 w-[400px]' type="password" name="password" />
-            </label>
-            <button className='bg-[#d9d9d9] rounded-lg h-[64px] text-lg'>로그인</button>
+            <div>
+                <label className='text-lg' htmlFor='email'>이메일</label>
+                <input id="email"className='border-2 border-[#d9d9d9] rounded-lg h-[60px] px-4 w-[400px] mt-4' type="email" name="email" placeholder='이메일을 입력하세요.'/>
+            </div>
+            <div>
+                <label className='text-lg ' htmlFor='password'>비밀번호</label>
+                <input id="password" className='border-2 border-[#d9d9d9] rounded-lg h-[60px] px-4 w-[400px] mt-4' type="password" name="password" />
+            </div>
+            <SubmitButton title='로그인'/>
         </form>
 
 
