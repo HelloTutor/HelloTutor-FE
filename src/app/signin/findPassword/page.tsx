5de13263022eabdef1938a68808217a11561a9b8
suhyunH <87814333+suhyunH@users.noreply.github.com>
2023-12-29 23:06:46 +0900

    feat: 비밀번호 찾기 구현

--- src/app/signin/findPassword/page.tsx
+++ src/app/signin/findPassword/page.tsx
@@ -1,3 +1,28 @@
+import SubmitButton from '@/components/SubmitButton';
+
 export default function FindPassword() {
-    return(<></>)
+    return(
+    
+    <>
+    <p className='text-5xl my-10 text-center'>비밀번호 찾기</p>
+    <div className='bg-[#fdfbfb] w-[520px] py-[60px]  mx-auto '>
+
+        <form className='flex flex-col gap-y-12 max-w-[400px] mx-auto'>
+            <label>
+                <label className='ml-2 mb-3' htmlFor='email'>가입한 이메일 주소를 입력해 주세요.</label>
+                <input type="text" id="email"  name="email" placeholder="example@hellotutor.com" className='border-2 border-[#d9d9d9] rounded-lg h-[60px] px-4 w-[400px] shadow-md'></input>
+            </label>   
+            <div>
+                <p>가입하신 이메일 주소를 입력해 주시면</p>
+                <p>새로운 비밀번호를 설정 가능 링크를 보내드립니다.</p>
+            </div>
+          <SubmitButton title='이메일 전송하기'/>
+        </form>
+            
+     
+    
+    
+    </div>
+    </>
+    )
 }
