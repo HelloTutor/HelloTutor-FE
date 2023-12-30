64b943c49d887bfc3195b60953a3f2f56a81104b
suhyunH <87814333+suhyunH@users.noreply.github.com>
2023-12-30 12:24:05 +0900

    refactor: vscode setting 및 gitignore 추가

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,23 +1,25 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-export default function Signup() {  
+export default function Signup() {
+  const signupType = [
+    { name: "일반회원", link: "/signup/student" },
+    { name: "튜터", link: "/signup/tutor" },
+  ];
 
-    const signupType =[{name:'일반회원',link:'/signup/student'},{name:'튜터',link:'/signup/tutor'}]
-    return(
-    
-    <div className='text-center mt-[160px]'>
-        <p className='text-5xl mb-20'>회원가입</p>
-    <div className='flex gap-10 justify-center'>
-    
-    {signupType.map((item)=>(
-        
-        <Link className='w-[300px] h-[300px] bg-[#d9d9d9] flex items-center  justify-center' key={item.name} href={item.link}>
-          <span className=' font-semibold text-5xl'>{item.name}</span>
-        </Link>
-
-))}
-
+  return (
+    <div className="text-center mt-[160px]">
+      <p className="text-5xl mb-20">회원가입</p>
+      <div className="flex gap-10 justify-center">
+        {signupType.map((item) => (
+          <Link
+            className="w-[300px] h-[300px] bg-[#d9d9d9] flex items-center  justify-center"
+            key={item.name}
+            href={item.link}
+          >
+            <span className=" font-semibold text-5xl">{item.name}</span>
+          </Link>
+        ))}
+      </div>
     </div>
-</div>
-    
-    )}
+  );
+}
