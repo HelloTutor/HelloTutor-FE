5d4b3b07c4af06b4548f613f9011e69d571c06ab
suhyunH <87814333+suhyunH@users.noreply.github.com>
2023-12-30 12:23:39 +0900

    refactor: subject constants 폴더로 이동

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,72 +1,79 @@
-'use client';
-import { ArrowRightCircleIcon } from '@heroicons/react/24/outline';
-import { ArrowLeftCircleIcon } from '@heroicons/react/24/outline';
-import Slider, { Settings } from 'react-slick';
+"use client";
+import { subjectArr } from "@/constants/subject";
+import { ArrowRightCircleIcon } from "@heroicons/react/24/outline";
+import { ArrowLeftCircleIcon } from "@heroicons/react/24/outline";
+import Slider, { Settings } from "react-slick";
 export default function Home() {
+  const settings: Settings = {
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    prevArrow: (
+      <div>
+        <ArrowLeftCircleIcon className="h-10 w-10 absolute z-10 top-[15px] left-[-25px] text-[#323232]" />
+      </div>
+    ),
+    nextArrow: (
+      <div>
+        <ArrowRightCircleIcon className="h-10 w-10 absolute z-10 top-[15px] right-[-15px] text-[#323232]" />
+      </div>
+    ),
+  };
 
-  const subjectArr = ['국어','영어','수학','사회','과학'];
-
-  
-    const settings: Settings = {
-      infinite: true,
-      speed: 1000,
-      slidesToShow: 5,
-      slidesToScroll: 5,
-      prevArrow:<div><ArrowLeftCircleIcon className='h-10 w-10 absolute z-10 top-[15px] left-[-25px] text-[#323232]'/></div>,
-      nextArrow:<div><ArrowRightCircleIcon className='h-10 w-10 absolute z-10 top-[15px] right-[-15px] text-[#323232]'/></div>
-  
-    
-    };
-  
-
-  return <div className=' max-w-[970px] mx-auto'>
-  {/* 헤더 이미지 */}
-  <div className=" bg-black w-full h-72"/>
-  {/* 과목 이미지 */}
-  <div className='flex gap-x-6 my-16 justify-center'>
-    {subjectArr.map((subject) => (
-    <div key={subject}>
-      <div className='bg-black w-[123px] h-[123px]'/>
-      <p className='mt-2 text-center'>{subject}</p>
-    </div>
-    ))}
-  </div>
-  {/* 지금 인기있는 tutor */}
-  <div className='mb-16'>
-    <p className="ht-h2-title">지금 인기있는 tutor</p>
-    {/* subject filter banners 추후 components화 */}
-    <div className='flex gap-x-5 mt-6 ml-8'>
-      {subjectArr.map((subject) => (  
-        <button className='bg-[#d9d9d9] rounded-3xl px-4 py-1' key={subject}>
-        <p>{subject}</p>
-      </button>
-      ))}
-    </div>
-
-      {/* tutor slide */}      
-    <Slider    {...settings} className='max-w-[900px] pt-10 mx-auto relative'>
-      {[1,2,3,4,5,6,7,8,9,10,11,12].map((number)=>(
-        <div className=' bg-slate-400 max-w-[168px] h-[168px]' key={number}>
-          <span>{number}</span>
-        </div>
+  return (
+    <div className=" max-w-[970px] mx-auto">
+      {/* 헤더 이미지 */}
+      <div className=" bg-black w-full h-72" />
+      {/* 과목 이미지 */}
+      <div className="flex gap-x-6 my-16 justify-center">
+        {subjectArr.map((subject) => (
+          <div key={subject}>
+            <div className="bg-black w-[123px] h-[123px]" />
+            <p className="mt-2 text-center">{subject}</p>
+          </div>
         ))}
-    </Slider>
-  </div>
-  {/* 실시간 질문 */}
-  <div className='mb-16'>
-    <p className='ht-h2-title'>실시간 질문</p>
-    <div className='flex gap-x-5 mt-6 ml-8'>
-      {subjectArr.map((subject) => (  
-        <button className='bg-[#d9d9d9] rounded-3xl px-4 py-1' key={subject}>
-        <p>{subject}</p>
-      </button>
-      ))}
+      </div>
+      {/* 지금 인기있는 tutor */}
+      <div className="mb-16">
+        <p className="ht-h2-title">지금 인기있는 tutor</p>
+        {/* subject filter banners 추후 components화 */}
+        <div className="flex gap-x-5 mt-6 ml-8">
+          {subjectArr.map((subject) => (
+            <button
+              className="bg-[#d9d9d9] rounded-3xl px-4 py-1"
+              key={subject}
+            >
+              <p>{subject}</p>
+            </button>
+          ))}
+        </div>
+
+        {/* tutor slide */}
+        <Slider {...settings} className="max-w-[900px] pt-10 mx-auto relative">
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((number) => (
+            <div className=" bg-slate-400 max-w-[168px] h-[168px]" key={number}>
+              <span>{number}</span>
+            </div>
+          ))}
+        </Slider>
+      </div>
+      {/* 실시간 질문 */}
+      <div className="mb-16">
+        <p className="ht-h2-title">실시간 질문</p>
+        <div className="flex gap-x-5 mt-6 ml-8">
+          {subjectArr.map((subject) => (
+            <button
+              className="bg-[#d9d9d9] rounded-3xl px-4 py-1"
+              key={subject}
+            >
+              <p>{subject}</p>
+            </button>
+          ))}
+        </div>
+      </div>
+      {/* 오늘의 인기있는 글 */}
+      <p className="ht-h2-title">오늘의 인기있는 글</p>
     </div>
-  </div>
-  {/* 오늘의 인기있는 글 */}
-  <p className='ht-h2-title'>오늘의 인기있는 글</p>
-  
-  
-  
-  </div>;
+  );
 }
---
+++ src/constants/subject.ts
@@ -0,0 +1,9 @@
+export const subjectArr = ["국어", "영어", "수학", "사회", "과학"];
+
+export const subjectKoEn = [
+  { ko: "국어", en: "korean" },
+  { ko: "영어", en: "english" },
+  { ko: "수학", en: "math" },
+  { ko: "사회", en: "social" },
+  { ko: "과학", en: "science" },
+];
