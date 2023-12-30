6beae9633b0c7ec58af0d550a3b8b21ccad91db0
suhyunH <87814333+suhyunH@users.noreply.github.com>
2023-12-30 12:22:56 +0900

    feat: useForm 적용

--- src/app/signup/student/page.tsx
+++ src/app/signup/student/page.tsx
@@ -1,67 +1,130 @@
-'use client';
-import SubmitButton from '@/components/SubmitButton';
-import { useState } from 'react';
+"use client";
+import SubmitButton from "@/components/SubmitButton";
+import { SubmitHandler, useForm } from "react-hook-form";
 
-export default function Student(){
+interface UserDataTypes {
+  email: string;
+  password: string;
+  confirm_password: string;
+  name: string;
+}
+export default function Student() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<UserDataTypes>({
+    mode: "onChange",
+    defaultValues: {
+      email: "",
+      password: "",
+      confirm_password: "",
+      name: "",
+    },
+  });
+  const onSubmitHandler: SubmitHandler<UserDataTypes> = (data) =>
+    console.log(data);
 
-    // 고민.. 유효성 검사는 yupResolver를 사용할까? 아니면 직접 구현할까?
-
-    
-    const [userData, setUserData] = useState({
-        email:'',
-        password:'',
-        confirm_password:'',
-        name:''
-    })
-
-    const handleInput = (e:React.ChangeEvent<HTMLFormElement>) =>{
-        const {name, value} = e.target;
-        setUserData({
-            ...userData,
-            [name]:value
-        })
-    }
-
-    const {email, password, confirm_password, name} = userData;
-
-
-    return(
-    
+  return (
     <>
-    {/* 회원가입 */}
-    <p className='text-5xl my-10 text-center'>회원가입</p>
-    <div className='w-[520px] py-[60px]  mx-auto'>
+      {/* 회원가입 */}
+      <p className="text-5xl my-10 text-center">회원가입</p>
+      <div className="w-[520px] py-[60px]  mx-auto">
+        <form
+          className="flex flex-col gap-y-8 max-w-[400px] mx-auto"
+          onSubmit={handleSubmit(onSubmitHandler)}
+        >
+          <div>
+            <label className="ml-2 mb-3" htmlFor="email">
+              이메일(ID)
+            </label>
+            <input
+              id="email"
+              placeholder="이메일을 입력해주세요."
+              className="border-2 border-[#d9d9d9] rounded-lg h-[60px] px-4 w-[400px] shadow-md"
+              {...register("email", {
+                required: "이메일은 필수 값입니다.",
+                pattern: {
+                  value: /^[a-zA-Z0-9_\.\.-]+@[a-zA-Z0-9]+\.[A-Za-z]+$/,
+                  message: "이메일 형식이 올바르지 않습니다.",
+                },
+              })}
+            />
+            <p className="relative text-red-500 h-[10px]">
+              {errors?.email?.message}
+            </p>
+          </div>
+          <div>
+            <label className="ml-2 mb-3" htmlFor="password">
+              비밀번호
+            </label>
+            <input
+              id="password"
+              type="password"
+              placeholder="비밀번호를 입력해주세요."
+              className="border-2 border-[#d9d9d9] rounded-lg h-[60px] px-4 w-[400px] shadow-md"
+              {...register("password", {
+                required: "비밀번호는 필수 값입니다.",
+                minLength: {
+                  value: 8,
+                  message: "8자리 이상 비밀번호를 사용하세요",
+                },
+                pattern: {
+                  value: /^[a-zA-Z0-9]{8,10}/,
+                  message: "영문, 숫자 포함 8 ~ 10자로 입력해주세요",
+                },
+              })}
+            />
+            <p className="relative text-red-500 h-[10px]">
+              {errors?.password?.message}
+            </p>
+          </div>
+          <div>
+            <label className="ml-2 mb-3" htmlFor="confirm_password">
+              비밀번호 확인
+            </label>
+            <input
+              id="confirm_password"
+              type="password"
+              placeholder="비밀번호를 다시 입력해주세요."
+              className="border-2 border-[#d9d9d9] rounded-lg h-[60px] px-4 w-[400px] shadow-md"
+              {...register("confirm_password", {
+                required: "비밀번호를 확인해주세요",
+                validate: {
+                  checkPassword: (value) =>
+                    value === watch("password") ||
+                    "비밀번호가 일치하지 않습니다.",
+                },
+              })}
+            />
+            <p className="relative text-red-500 h-[10px]">
+              {errors?.confirm_password?.message}
+            </p>
+          </div>
+          <div>
+            <label className="ml-2 mb-3">이름</label>
+            <input
+              id="name"
+              type="text"
+              placeholder="이름을 입력해주세요."
+              className="border-2 border-[#d9d9d9] rounded-lg h-[60px] px-4 w-[400px] shadow-md"
+              {...register("name", {
+                required: "이름은 필수 값입니다.",
+              })}
+            />
+            <p className="relative text-red-500 h-[10px]">
+              {errors?.name?.message}
+            </p>
+          </div>
 
-        <form className='flex flex-col gap-y-8 max-w-[400px] mx-auto' onChange={handleInput}>
-            <div>
-                <label className='ml-2 mb-3' htmlFor='email'>이메일(ID)</label>
-                <input id="email" name="email" value={email} type="text" placeholder="이메일을 입력해주세요." className='border-2 border-[#d9d9d9] rounded-lg h-[60px] px-4 w-[400px] shadow-md'></input>
-            </div>
-            <div>
-
-                <label className='ml-2 mb-3' htmlFor='password'>비밀번호</label>
-                <input id="password" name="password" value={password} type="password" placeholder="비밀번호를 입력해주세요." className='border-2 border-[#d9d9d9] rounded-lg h-[60px] px-4 w-[400px] shadow-md'></input>
-            </div>
-            <div>
-
-                <label className='ml-2 mb-3' htmlFor='confirm_password'>비밀번호 확인</label>
-                <input id="confirm_password" value={confirm_password} name="confirm_password" type="password" placeholder="비밀번호를 다시 입력해주세요." className='border-2 border-[#d9d9d9] rounded-lg h-[60px] px-4 w-[400px] shadow-md'></input>
-                { userData.confirm_password &&userData.password !== userData.confirm_password && <p className='text-red-500'>비밀번호가 일치하지 않습니다.</p>}
-            </div>
-            <div>
-                <label className='ml-2 mb-3'>이름</label>
-                <input id="name" value={name} name="name" type="text" placeholder="이름을 입력해주세요." className='border-2 border-[#d9d9d9] rounded-lg h-[60px] px-4 w-[400px] shadow-md'></input>
-            </div>
-            <SubmitButton title="가입하기"/>
+          <SubmitButton title="가입하기" />
         </form>
-        <div className='my-4 flex flex-col gap-y-4  items-center'>
-
-            <SubmitButton title="카카오톡으로 가입하기"/>
-            <SubmitButton title="구글로 가입하기"/>
+        <div className="my-4 flex flex-col gap-y-4  items-center">
+          <SubmitButton title="카카오톡으로 가입하기" />
+          <SubmitButton title="구글로 가입하기" />
         </div>
-    </div>        
-
-        
+      </div>
     </>
-    )
+  );
 }
--- src/app/signup/tutor/page.tsx
+++ src/app/signup/tutor/page.tsx
@@ -1,85 +1,147 @@
-'use client'
-import SubmitButton from '@/components/SubmitButton';
-import { useState } from 'react';
+"use client";
+import SubmitButton from "@/components/SubmitButton";
+import { subjectKoEn } from "@/constants/subject";
+import { SubmitHandler, useForm } from "react-hook-form";
 
-export default function Tutor(){
-    const [userData, setUserData] = useState({
-        email:'',
-        password:'',
-        confirm_password:'',
-        name:'',
-        subject:''
-    })
-    const handleInput = (e:React.ChangeEvent<HTMLFormElement>) =>{
-        const {name, value} = e.target;
-        setUserData({
-            ...userData,
-            [name]:value
-        })
-    }
+interface TutorDataTypes {
+  email: string;
+  password: string;
+  confirm_password: string;
+  name: string;
+  subject: string;
+}
 
-    const {email, password, confirm_password, name, } = userData;
+export default function Tutor() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<TutorDataTypes>({
+    mode: "onChange",
+    defaultValues: {
+      email: "",
+      password: "",
+      confirm_password: "",
+      name: "",
+      subject: "",
+    },
+  });
+  const onSubmitHandler: SubmitHandler<TutorDataTypes> = (data) =>
+    console.log(data);
 
-    return(<>
-    
-    <p className='text-5xl my-10 text-center'>회원가입</p>
-    <div className='w-[520px] py-[60px]  mx-auto'>
-
-        <form className='flex flex-col gap-y-8 max-w-[400px] mx-auto' onChange={handleInput}>
-            <div>
-                <label className='ml-2 mb-3' htmlFor='email'>이메일(ID)</label>
-                <input id="email" name="email" value={email} type="text" placeholder="이메일을 입력해주세요." className='border-2 border-[#d9d9d9] rounded-lg h-[60px] px-4 w-[400px] shadow-md'></input>
-            </div>
-            <div>
-
-                <label className='ml-2 mb-3' htmlFor='password'>비밀번호</label>
-                <input id="password" name="password" value={password} type="password" placeholder="비밀번호를 입력해주세요." className='border-2 border-[#d9d9d9] rounded-lg h-[60px] px-4 w-[400px] shadow-md'></input>
-            </div>
-            <div>
-
-                <label className='ml-2 mb-3' htmlFor='confirm_password'>비밀번호 확인</label>
-                <input id="confirm_password" value={confirm_password} name="confirm_password" type="password" placeholder="비밀번호를 다시 입력해주세요." className='border-2 border-[#d9d9d9] rounded-lg h-[60px] px-4 w-[400px] shadow-md'></input>
-                { userData.confirm_password &&userData.password !== userData.confirm_password && <p className='text-red-500'>비밀번호가 일치하지 않습니다.</p>}
-            </div>
-            <div>
-                <label className='ml-2 mb-3'>이름</label>
-                <input id="name" value={name} name="name" type="text" placeholder="이름을 입력해주세요." className='border-2 border-[#d9d9d9] rounded-lg h-[60px] px-4 w-[400px] shadow-md'></input>
-            </div>
-            <div>
-                <label className='ml-2'>활동분야</label>
-                <div className='flex gap-x-4 justify-center pt-3'>
-                    <div>
-                        <input type="radio" id="korean" name="subject" value="korean"/>
-                        <label htmlFor="korean">국어</label>
-                    </div>
-                    <div>
-                        <input type="radio" id="english" name="subject" value="english"/>
-                        <label htmlFor="english">영어</label>
-                    </div>
-                    <div>
-                        <input type="radio" id="math" name="subject" value="math"/>
-                        <label htmlFor="math">수학</label>
-                    </div>
-                    <div>
-                        <input type="radio" id="social" name="subject" value="social"/>
-                        <label htmlFor="social">사회</label>
-                    </div>
-                    <div>
-                        <input type="radio" id="science" name="subject" value="science"/>
-                        <label htmlFor="science">과학</label>
-                    </div>
+  return (
+    <>
+      <p className="text-5xl my-10 text-center">회원가입</p>
+      <div className="w-[520px] py-[60px]  mx-auto">
+        <form
+          className="flex flex-col gap-y-8 max-w-[400px] mx-auto"
+          onSubmit={handleSubmit(onSubmitHandler)}
+        >
+          <div>
+            <label className="ml-2 mb-3" htmlFor="email">
+              이메일(ID)
+            </label>
+            <input
+              id="email"
+              placeholder="이메일을 입력해주세요."
+              className="border-2 border-[#d9d9d9] rounded-lg h-[60px] px-4 w-[400px] shadow-md"
+              {...register("email", {
+                required: "이메일은 필수 값입니다.",
+                pattern: {
+                  value: /^[a-zA-Z0-9_\.\.-]+@[a-zA-Z0-9]+\.[A-Za-z]+$/,
+                  message: "이메일 형식이 올바르지 않습니다.",
+                },
+              })}
+            />
+            <p className="relative text-red-500 h-[10px]">
+              {errors?.email?.message}
+            </p>
+          </div>
+          <div>
+            <label className="ml-2 mb-3" htmlFor="password">
+              비밀번호
+            </label>
+            <input
+              id="password"
+              type="password"
+              placeholder="비밀번호를 입력해주세요."
+              className="border-2 border-[#d9d9d9] rounded-lg h-[60px] px-4 w-[400px] shadow-md"
+              {...register("password", {
+                required: "비밀번호는 필수 값입니다.",
+                minLength: {
+                  value: 8,
+                  message: "8자리 이상 비밀번호를 사용하세요",
+                },
+                pattern: {
+                  value: /^[a-zA-Z0-9]{8,10}/,
+                  message: "영문, 숫자 포함 8 ~ 10자로 입력해주세요",
+                },
+              })}
+            />
+            <p className="relative text-red-500 h-[10px]">
+              {errors?.password?.message}
+            </p>
+          </div>
+          <div>
+            <label className="ml-2 mb-3" htmlFor="confirm_password">
+              비밀번호 확인
+            </label>
+            <input
+              id="confirm_password"
+              type="password"
+              placeholder="비밀번호를 다시 입력해주세요."
+              className="border-2 border-[#d9d9d9] rounded-lg h-[60px] px-4 w-[400px] shadow-md"
+              {...register("confirm_password", {
+                required: "비밀번호를 확인해주세요",
+                validate: {
+                  checkPassword: (value) =>
+                    value === watch("password") ||
+                    "비밀번호가 일치하지 않습니다.",
+                },
+              })}
+            />
+            <p className="relative text-red-500 h-[10px]">
+              {errors?.confirm_password?.message}
+            </p>
+          </div>
+          <div>
+            <label className="ml-2 mb-3">이름</label>
+            <input
+              id="name"
+              type="text"
+              placeholder="이름을 입력해주세요."
+              className="border-2 border-[#d9d9d9] rounded-lg h-[60px] px-4 w-[400px] shadow-md"
+              {...register("name", {
+                required: "이름은 필수 값입니다.",
+              })}
+            />
+            <p className="relative text-red-500 h-[10px]">
+              {errors?.name?.message}
+            </p>
+          </div>
+          <div>
+            <label className="ml-2">활동분야</label>
+            <div className="flex gap-x-4 justify-center pt-3">
+              {subjectKoEn.map((subject) => (
+                <div key={subject.en}>
+                  <input
+                    type="radio"
+                    id={subject.en}
+                    value={subject.en}
+                    {...register("subject")}
+                  />
+                  <label htmlFor={subject.en}>{subject.ko}</label>
                 </div>
+              ))}
             </div>
+          </div>
 
-            {/* 휴대전화 인증*/}
-            
+          {/* 휴대전화 인증*/}
 
-          
-
-            <SubmitButton title="가입하기"/>
+          <SubmitButton title="가입하기" />
         </form>
-        
-    </div>        
-    
-    </>)
-}   
+      </div>
+    </>
+  );
+}
