e4cc53cc15d6785412208db2a5e7dbdd60917ea1
seokhochoi <seokho9473@gmail.com>
2024-01-04 16:44:47 +0900

    게시글의 렌더링 기준을 나누어주는 DashboardNavBar 컴포넌트 생성

--- src/app/question/page.tsx
+++ src/app/question/page.tsx
@@ -1,5 +1,29 @@
+'use client';
+
+import DashboardNavBar from '@/components/DashboardNavBar';
 import SearchForm from '@/components/SearchForm';
 import SubjectSideBar from '@/components/SubjectSideBar';
+
+const navItemMap = [
+  {
+    navTitle: '전체',
+    navi() {
+      console.log('전체');
+    },
+  },
+  {
+    navTitle: '미해결',
+    navi() {
+      console.log('미해결');
+    },
+  },
+  {
+    navTitle: '해결',
+    navi() {
+      console.log('해결');
+    },
+  },
+];
 
 export default function Question() {
   return (
@@ -9,6 +33,7 @@ export default function Question() {
 
       {/* content right */}
       <section className='flex flex-col ml-[69px] mt-20'>
+        <DashboardNavBar navItem={navItemMap} />
         <SearchForm />
       </section>
     </div>
