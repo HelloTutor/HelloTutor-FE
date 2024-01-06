3f34425e8b31114d5cf017e123569ddab7153ea8
suhyunH <87814333+suhyunH@users.noreply.github.com>
2024-01-06 13:50:50 +0900

    feat: submit시 약관동의 상태는 프론트에서 유효성 검사만 하도록 변경

--- src/app/signup/tutor/page.tsx
+++ src/app/signup/tutor/page.tsx
@@ -2,6 +2,7 @@
 import AgreeOfTos from "@/components/ArgreeOfTos";
 import SubmitButton from "@/components/SubmitButton";
 import { subjectKoEn } from "@/constants/subject";
+import { useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 
 export interface TutorDataTypes {
@@ -10,7 +11,6 @@ export interface TutorDataTypes {
   confirm_password: string;
   name: string;
   subjects: string[];
-  policy: boolean;
 }
 
 export default function Tutor() {
@@ -19,11 +19,18 @@ export default function Tutor() {
     handleSubmit,
     watch,
     formState: { errors },
-  } = useForm<TutorDataTypes>();
+  } = useForm<TutorDataTypes>({ mode: "onChange" });
 
-  const onSubmitHandler: SubmitHandler<TutorDataTypes> = (data) =>
+  const [policy, setPolicy] = useState(false);
+  const [policyError, setPolicyError] = useState(false);
+
+  const onSubmitHandler: SubmitHandler<TutorDataTypes> = (data) => {
+    if (!policy) {
+      setPolicyError(true);
+      return;
+    }
     console.log(data);
-
+  };
   return (
     <>
       <p className="text-5xl my-10 text-center">회원가입</p>
@@ -133,7 +140,11 @@ export default function Tutor() {
           </div>
 
           {/* 약관 동의 */}
-          <AgreeOfTos register={register} error={errors} />
+          <AgreeOfTos
+            policy={policy}
+            setPolicy={setPolicy}
+            policyError={policyError}
+          />
           <SubmitButton title="가입하기" />
         </form>
       </div>
