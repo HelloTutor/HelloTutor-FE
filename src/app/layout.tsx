74a2994c92b5c9d05c8437774bdd596df4d34805
suhyunH <87814333+suhyunH@users.noreply.github.com>
2024-01-08 17:01:39 +0900

    feat: footer 생성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,15 @@ export default function RootLayout({
         </header>
 
         {children}
+
+        <footer className="relative bottom-0 w-full my-[50px] text-lg">
+          <hr className="mb-[40px]  border-black" />
+          <div className="flex flex-col gap-y-1 ml-6">
+            <p className="text-lg font-bold">고객센터</p>
+            <Link href="/faq">자주 묻는 질문</Link>
+            <Link href="/support">문의하기</Link>
+          </div>
+        </footer>
       </body>
     </html>
   );
