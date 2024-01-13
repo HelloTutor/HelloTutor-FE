26c354ef86fdf38a3b8e5b5d03c1734d6fa36a74
seokhochoi <seokho9473@gmail.com>
2024-01-13 23:22:03 +0900

    feat: Support, TextField, FileUpload 생성

--- src/app/support/page.tsx
+++ src/app/support/page.tsx
@@ -1,3 +1,39 @@
+'use client';
+
+import FileUpload from '@/components/FileUpload';
+import TextField from '@/components/TextField';
+
 export default function Support() {
-  return <></>;
+  return (
+    <>
+      <TextField
+        title='이메일 주소'
+        placeholder='ex) helloTutor@gmail.com'
+        required={true}
+      />
+      <TextField
+        title='제목'
+        placeholder='ex) 선형대수학 문의합니다.'
+        required={true}
+      />
+      <TextField
+        title='설명'
+        type='textarea'
+        placeholder='이곳에 내용을 적어주세요!'
+        required={true}
+      />
+      <TextField
+        title='HelloTutor 가입 이메일'
+        placeholder='ex) helloTutor@gmail.com'
+        required={true}
+      />
+      <TextField
+        title='HelloTutor 가입 휴대폰 번호'
+        placeholder='ex) 010-1212-1213'
+        required={true}
+      />
+
+      <FileUpload />
+    </>
+  );
 }
