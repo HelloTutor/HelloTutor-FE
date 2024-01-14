102151bb8a8cd72fb997323bfea64392e751c4c0
seokhochoi <seokho9473@gmail.com>
2024-01-15 00:47:19 +0900

    feat: pagenation 초기 구현 및 question, dashboard 페이지에 적용

--- src/components/Pagenation/index.tsx
+++ src/components/Pagenation/index.tsx
@@ -2,7 +2,7 @@ import Pagination from 'react-js-pagination';
 
 export default function Pagenation() {
   return (
-    <div>
+    <div className='flex justify-center'>
       <Pagination
         // 현재 페이지
         activePage={1}
@@ -13,6 +13,12 @@ export default function Pagenation() {
         // 표시할 총 페이지 수
         pageRangeDisplayed={5}
         onChange={() => {}}
+        // 페이징 버튼 내부에 적용될 CSS 클래스를 지정
+        innerClass='flex justify-center'
+        // 페이지 아이템에 적용될 CSS 클래스 지정 (배경 입힐시 bg-gray-500 hover:bg-gray-700 사용)
+        itemClass='px-2 text-black hover:text-gray-600 rounded'
+        // 링크에 적용될 CSS 클래스 지정
+        linkClass='text-lg'
       ></Pagination>
     </div>
   );
