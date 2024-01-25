e62791033ba14cb017e81b5c4ada1924a4374d3f
seokhochoi <seokho9473@gmail.com>
2024-01-25 13:53:34 +0900

    chore: 글작성 테스트 제거

--- src/app/dashboard/write-content/page.tsx
+++ src/app/dashboard/write-content/page.tsx
@@ -6,39 +6,7 @@ import { useState } from 'react';
 
 export default function WriteContent() {
   const [html, setHtml] = useState('');
-  console.log('test');
-  const testApi = async () => {
-    const data = await writeContent('/free', {
-      title: '테스트',
-      content: [
-        {
-          type: 'p',
-          attributes: {},
-          children: [{ type: 'text', textContent: 'asdf' }],
-        },
-        {
-          type: 'p',
-          attributes: {},
-          children: [{ type: 'br', attributes: {}, children: [] }],
-        },
-        {
-          type: 'p',
-          attributes: {},
-          children: [
-            { type: 'text', textContent: 'asdf' },
-            {
-              type: 'span',
-              attributes: { style: 'color: rgb(230, 0, 0);' },
-              children: [{ type: 'text', textContent: 'test' }],
-            },
-          ],
-        },
-      ],
-    });
 
-    console.log(data);
-  };
-  testApi();
   return (
     <div>
       <QuestionWriting setHtml={setHtml} html={html} />
