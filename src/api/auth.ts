e67ba0ef0acbe6c7c4df2b458814e83989aa0a6f
seokhochoi <seokho9473@gmail.com>
2024-01-25 13:37:55 +0900

    feat: headers 토큰 추가 위한 apiClientWithInterceptor 생성

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,24 +1,33 @@
-import { TutorDataTypes } from "@/typings/user";
-import apiClient from "@/utils/api-client";
+import { TutorDataTypes } from '@/typings/user';
+import apiClient, { apiClientWithInterceptor } from '@/utils/api-client';
 
 export const signupTutor = async (userParms: TutorDataTypes) => {
   return await apiClient
-    .post("/auth/user/tutor", userParms)
+    .post('/auth/user/tutor', userParms)
     .then((data) => data.data);
 };
 
 export const signupTutee = async (
-  userParms: Omit<TutorDataTypes, "subject">
+  userParms: Omit<TutorDataTypes, 'subject'>
 ) => {
   return await apiClient
-    .post("/auth/user/tutee", userParms)
+    .post('/auth/user/tutee', userParms)
     .then((data) => data.data);
 };
 
 export const signin = async (
-  userParms: Pick<TutorDataTypes, "email" | "pw">
+  userParms: Pick<TutorDataTypes, 'email' | 'pw'>
 ) => {
   return await apiClient
-    .post("/auth/login", userParms)
+    .post('/auth/login', userParms)
+    .then((data) => data.data);
+};
+
+export const writeContent = async (
+  path: string,
+  params: { title: string; content: Array<Object> }
+) => {
+  return await apiClientWithInterceptor
+    .post(path, params)
     .then((data) => data.data);
 };
--- src/app/dashboard/write-content/page.tsx
+++ src/app/dashboard/write-content/page.tsx
@@ -1,11 +1,44 @@
 'use client';
 
+import { writeContent } from '@/api/auth';
 import QuestionWriting from '@/components/QuestionWriting';
 import { useState } from 'react';
 
 export default function WriteContent() {
   const [html, setHtml] = useState('');
+  console.log('test');
+  const testApi = async () => {
+    const data = await writeContent('/free', {
+      title: '테스트',
+      content: [
+        {
+          type: 'p',
+          attributes: {},
+          children: [{ type: 'text', textContent: 'asdf' }],
+        },
+        {
+          type: 'p',
+          attributes: {},
+          children: [{ type: 'br', attributes: {}, children: [] }],
+        },
+        {
+          type: 'p',
+          attributes: {},
+          children: [
+            { type: 'text', textContent: 'asdf' },
+            {
+              type: 'span',
+              attributes: { style: 'color: rgb(230, 0, 0);' },
+              children: [{ type: 'text', textContent: 'test' }],
+            },
+          ],
+        },
+      ],
+    });
 
+    console.log(data);
+  };
+  testApi();
   return (
     <div>
       <QuestionWriting setHtml={setHtml} html={html} />
